ced7ef8eeb0e334a74225037d4b04b9a19e439fc
messay f <info@messay.com>
2018-09-08 01:30:11 -0500

    social icons

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -16,6 +16,15 @@ const Layout = ({ children, data }) => (
     All of my work is based on applying the design process to a variety of problems. 
       </p>
       </div>
+      <div className="social">
+     <a href="https://www.instagram.com/messay_d/"><img src={require ('../images/insta.svg')} width="20"/></a>
+     <a href="https://dribbble.com/messay_design"><img src={require ('../images/dribbble.svg')} width="20"/></a>
+     <a href="https://twitter.com/messay_co"><img src={require ('../images/twitter.svg')} width="20"/></a>
+     <a href="/"><img src={require ('../images/medium.svg')} width="20"/></a>
+     <a href="www.linkedin.com/in/messayf"><img src={require ('../images/linkedin.svg')} width="20"/></a>
+     <a href="https://www.behance.net/messayco"><img src={require ('../images/behance.svg')} width="100"/></a>
+     <a href="/"><img src={require ('../images/github.svg')} width="20"/></a>
+     </div>
 </div>
     </div>
   </div>
