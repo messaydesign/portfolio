a05d25199272bbdb810e61c597a26e5c2ce58494
messay f <info@messay.com>
2018-09-08 12:59:43 -0500

    sectio for ipad and phone

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -31,6 +31,24 @@ const Layout = ({ children, data }) => (
      </div>
 </div>
     </div>
+    <div className="section">
+    <div className="sectionGroup">
+    <h1>
+  Let’s Build <br/> Something great together 👍🏽
+  </h1>
+  <p>
+  It is all about solving problems, if not it would be pixel art. Product design always starts with understanding the problem rigorously.The fun part being a product designer is that I have problems to solve every single day with collaborative problem-solving process. In my 7+ years journey I realize that close collaboration of multidisciplinary team with a design process creates a winning product. 
+Each individual from different disciplinary provides a unique and valuable perspective about the problem, as product enhances and feature updates have a huge impact across internal and external products.   In my product design process, I’ll be focusing on four areas I believe will facilitate my ability to make meaningful contributions to the team I am working with.
+</p>
+
+    
+    
+    </div>
+    
+    </div>
+
+
+
   </div>
 )
 
