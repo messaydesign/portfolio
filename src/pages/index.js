a273f7121352bb017d8b7580421be6d04178c0c7
messay f <info@messay.com>
2018-09-08 00:01:15 -0500

    zoom disable

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Link from 'gatsby-link'
-
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 const IndexPage = () => (
   <div>
     <h1>Hi people</h1>
