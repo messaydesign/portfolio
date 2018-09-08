ec3b09a5401cd1fb0ac0fb777d636b92380f60ac
messay f <info@messay.com>
2018-09-08 02:00:55 -0500

    amharic

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -8,13 +8,17 @@ import './index.css'
 const Layout = ({ children, data }) => (
   <div>
     <div className="hero">
+    
     <div className="heroGroup">
+    
    <div className="name"> <h1> <img src={require ('../images/Logo.svg')}/>  Messay F. Denbel</h1>
+   <div class name="amharic"> <h4> መሣይ ፈለቀ ደንበል</h4></div>
    </div>
    <div className="nameCont"><p> <strong>👋🏽 Hi,</strong> <br/>I am a product designer focused on creating digital experiences who has a passion for motion, 
     visual design, human-centered designer. I really enjoy digging into complex user experience problems with a deliberate curious mindset. 
     All of my work is based on applying the design process to a variety of problems. 
       </p>
+    
       </div>
       <div className="social">
      <a href="https://www.instagram.com/messay_d/"><img src={require ('../images/insta.svg')} width="20"/></a>
