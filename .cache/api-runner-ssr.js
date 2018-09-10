6a39aaf9a3f45132b307e043078d92462905868a
messay <info@messay.design>
2018-09-10 09:47:40 -0500

    imGE FIX

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -1,5 +1,5 @@
 var plugins = [{
-      plugin: require('/Users/messaydenbel/Documents/portfolio/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
+      plugin: require('/Users/mdenbel/Documents/portfolio/node_modules/gatsby-plugin-react-helmet/gatsby-ssr'),
       options: {"plugins":[]},
     }]
 // During bootstrap, we write requires at top of this file which looks like:
--- .cache/layouts/index.js
+++ .cache/layouts/index.js
@@ -1,7 +1,7 @@
 
   import React from "react"
-  import Component from "/Users/messaydenbel/Documents/portfolio/src/layouts/index.js"
-  import data from "/Users/messaydenbel/Documents/portfolio/.cache/json/layout-index.json"
+  import Component from "/Users/mdenbel/Documents/portfolio/src/layouts/index.js"
+  import data from "/Users/mdenbel/Documents/portfolio/.cache/json/layout-index.json"
 
   export default (props) => <Component {...props} {...data} />
   
