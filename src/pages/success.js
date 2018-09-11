0eaecc1dc62637374042686caf8bb4387c4325f5
messay <info@messay.design>
2018-09-11 16:47:09 -0500

    social fix on success

--- src/pages/success.js
+++ src/pages/success.js
@@ -11,9 +11,19 @@ const success = () => (
     <img src={require ('../images/suc.svg')} width="100"/> <br/><br/>  
       I recived your message and <br/>I will be contacting you shortly to follow-up.<br/><br/>
     </h1>
-     <div className="backButton">  <Link to="/"> <img src={require ('../images/back.svg')} width="50"/></Link>  </div>
+     <div className="backButton">  <Link to="/"> <img src={require ('../images/back.svg')} width="50"/></Link>  
+     <div className="social">
+     <a href="https://www.instagram.com/messay_d/"><img src={require ('../images/insta.svg')} width="20"/></a>
+     <a href="https://dribbble.com/messay_design"><img src={require ('../images/dribbble.svg')} width="20"/></a>
+     <a href="https://twitter.com/messay_co"><img src={require ('../images/twitter.svg')} width="20"/></a>
+     <a href="/"><img src={require ('../images/medium.svg')} width="20"/></a>
+     <a href="https://www.linkedin.com/in/messayf/"><img src={require ('../images/linkedin.svg')} width="20"/></a>
+     <a href="https://www.behance.net/messayco"><img src={require ('../images/behance.svg')} width="100"/></a>
+     <a href="/"><img src={require ('../images/github.svg')} width="20"/></a>
      </div>
-    
+     </div>
+     </div>
+     
     </div>
   
     </div>
