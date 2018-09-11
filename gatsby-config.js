719bbee8a415515abcdacbd0d711c1b3efa08abc
messay <info@messay.design>
2018-09-11 10:22:16 -0500

    meta data fix

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Messay\' Portfolio'
+    title: 'Messay\' Portfolio',
+    siteUrl: `https://www.messay.design`,
+    description: `Online portfolio for Messay f. Denbel`,
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
