4db4f706c29ffefaaef9d1bbd51427d5cee51716
messay <info@messay.design>
2018-09-11 11:57:28 -0500

    forms on about page

--- src/pages/index.js
+++ src/pages/index.js
@@ -96,26 +96,6 @@ Each individual from different disciplinary provides a unique and valuable persp
   </div>
 </div>
 
-<form action="/pages/success " name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" netlify >
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>   
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
-  </p>
-</form>
 
   </div>
 )
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -6,6 +6,28 @@ const SecondPage = () => (
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
     <Link to="/">Go back to the homepage</Link>
+    <form action="/pages/success " name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" netlify >
+  <p>
+    <label>Your Name: <input type="text" name="name" /></label>   
+  </p>
+  <p>
+    <label>Your Email: <input type="email" name="email" /></label>
+  </p>
+  <p>
+    <label>Your Role: <select name="role[]" multiple>
+      <option value="leader">Leader</option>
+      <option value="follower">Follower</option>
+    </select></label>
+  </p>
+  <p>
+    <label>Message: <textarea name="message"></textarea></label>
+  </p>
+  <p>
+    <button type="submit">Send</button>
+  </p>
+</form>
+
+
   </div>
 )
 
