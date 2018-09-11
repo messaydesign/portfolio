a94c843a8fc559f829f91698c8e20a8c05fae334
messay <info@messay.design>
2018-09-11 10:25:00 -0500

    meta images

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,6 +3,7 @@ module.exports = {
     title: 'Messay\' Portfolio',
     siteUrl: `https://www.messay.design`,
     description: `Online portfolio for Messay f. Denbel`,
+    image: '../images/hero.jpg',
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
