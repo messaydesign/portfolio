756705a38cb19633487fbacb99144c87971a6d65
messay <info@messay.design>
2018-09-11 10:58:17 -0500

    google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,32 @@ module.exports = {
   siteMetadata: {
     title: 'Messay\' Portfolio',
     siteUrl: `https://www.messay.design`,
-    description: `Online portfolio for Messay f. Denbel`,
-    image: '../images/meta-hero.jpg',
+    description: `Online portfolio for Messay F. Denbel`,
+    author: 'MessayDenbel',
+    image: './images/meta-hero.jpg',
   },
   plugins: ['gatsby-plugin-react-helmet'],
+}
+
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-101394601-1",
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Any additional create only fields (optional)
+        sampleRate: 5,
+        siteSpeedSampleRate: 10,
+        cookieDomain: "messay.design",
+      },
+    },
+  ],
 }
