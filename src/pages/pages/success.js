4f6a91f3e0a8c9197e734d8ddf02e80d7638a29c
messay <info@messay.design>
2018-09-11 15:44:24 -0500

    mail sucees

--- src/pages/pages/success.js
+++ src/pages/pages/success.js
@@ -1,9 +1,24 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import './success.css'
 
 const success = () => (
   <div>
-    back to home 
+    <div className="success">
+    <div className="successGroup">
+    <div className="back">
+    <h1> 
+    <img src={require ('../pages/img/suc.svg')} width="100"/> <br/><br/>  
+      I recived your message and <br/>I will be contacting you shortly to follow-up.<br/><br/>
+    </h1>
+     <div className="backButton">  <Link to="/"> <img src={require ('../pages/img/back.svg')} width="50"/></Link>  </div>
+     </div>
+    
+    </div>
+  
+    </div>
+
+
   </div>
 )
 
